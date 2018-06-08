3b17ef9a2508467dae9a4535357ed9762d767418
hoxnox <hoxnox@gmail.com>
2018-06-08 12:26:46 +0300

    ENH: xcb-util-xrm added

---
+++ package/x11r7/xcb-util-xrm/xcb-util-xrm.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# xcb-util-keysyms
+#
+################################################################################
+
+XCB_UTIL_XRM_VERSION = 1.3
+XCB_UTIL_XRM_SOURCE = xcb-util-xrm-$(XCB_UTIL_XRM_VERSION).tar.bz2
+XCB_UTIL_XRM_SITE = https://github.com/Airblader/xcb-util-xrm/releases/download/v$(XCB_UTIL_XRM_VERSION)
+XCB_UTIL_XRM_INSTALL_STAGING = YES
+XCB_UTIL_XRM_DEPENDENCIES = libxcb
+
+$(eval $(autotools-package))
